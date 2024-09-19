2a0df8b3a7af70fa4e1d54361f98cbfff28ddbf6
GUILHERME DOS SANTOS BONATO <guilherme_s_bonato@estudante.sesisenai.org.br>
2024-09-19 20:38:10 -0300

    sla

---
+++ sla.py
@@ -0,0 +1,22 @@
+
+
+#imprime a string 'Olá, Mundo!'
+print("Ola, Mundo!") #Comendo em python
+
+
+
+#variaveis
+idade = 25
+nome = "ana"
+altura = 1.75
+estudante = True
+
+
+# imprime o nome e a idade
+print(f"Meu nome é {nome}, e tenho {idade} anos.")
+
+mensagem = "Cu de bebado não tem dono"
+print(len(mensagem))
+
+x = 10
+print(type(x))
